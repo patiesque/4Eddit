f7200295e8f108085813fb8cb4206590976d02e5
Christhopher Porpino <cgporpinof@gmail.com>
2020-01-20 13:52:57 -0300

    criei página de login

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -1,11 +1,42 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const Root = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 30vw;
+  height: 50vh;
+  justify-content: center;
+  align-items: center;
+`
+
+const InputUser = styled.input``
+
+const InputPassword = styled.input``
 
 class LoginPage extends Component {
   render() {
     return (
-      <div>
-        Oie
-      </div>
+      <Root>
+        <MainContainer>
+          <label>E-mail</label>
+          <InputUser>
+          </InputUser>
+          <label>Senha</label>
+          <InputPassword>
+          </InputPassword>
+          <button>Entrar</button>
+          <button>Cadastrar</button>
+        </MainContainer>
+      </Root>
     );
   }
 }
