d38e77faf2bfa7aa476b3671b22dd0b949e73401
Patricia <patymatiesque@gmail.com>
2020-01-25 17:01:31 -0300

    Entrega final

--- src/containers/LoginPage/index.js
+++ src/containers/LoginPage/index.js
@@ -4,12 +4,10 @@ import { connect } from "react-redux";
 import { login } from "../../action/login"
 import { routes } from "../Router";
 import { push } from "connected-react-router";
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import { CssBaseline } from "@material-ui/core";
